b05ca8f8d57703a81f650e5f6714b7932b00765a
Nathan7471 <54980608+mrnat7471@users.noreply.github.com>
2020-06-25 23:05:34 +0100

    streamers start

--- streamers.php
+++ streamers.php
@@ -7,15 +7,11 @@
             <h2 class="imagetext">Our Streamers</h2>
         </div>  
         <div class="our_Twitch">
-            <h1>Our Official Twitch Channel</h1>
-            <!-- Add a placeholder for the Twitch embed -->
-            <iframe
-                            src="https://player.twitch.tv/?channel=bhfevent&parent=bhf_public.test&muted=true"
-                            height="240"
-                            width="426"
-                            frameborder="0"
-                            scrolling="no"
-                            allowfullscreen="true"></iframe>
+            <h1>Our Official Twitch Channel</h1>   
+            <a href="https://twitch.tv/bhfevent" target="_blank"><div style="position: relative; left: 0; top: 0;">
+                <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_ooclanoo-640x360.jpg" class="bhfback"/>
+                <img src=".\img\streamers_icons\bhf.png" class="bhflogo"/>
+            </div></a> 
         </div>
         </div>
             <div class="partned_Streamers">
@@ -23,34 +19,25 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-sm">
-                            <p>Nathan7471</p>
-                        <iframe
-                            src="https://player.twitch.tv/?channel=nathan7471&parent=streamernews.example.com&muted=true"
-                            height="240"
-                            width="426"
-                            frameborder="0"
-                            scrolling="no"
-                            allowfullscreen="true"></iframe>
+                            <p>Shay_Gaming</p>
+                            <a href="https://twitch.tv/bhfevent" target="_blank"><div style="position: relative; left: 0; top: 0;">
+                                <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_ooclanoo-640x360.jpg" class="bhfback"/>
+                                <img src=".\img\streamers_icons\shay.jpg" class="bhflogo"/>
+                            </div></a>
                         </div>
                         <div class="col-sm">
                         <p>bluedk</p>
-                            <iframe
-                            src="https://player.twitch.tv/?channel=vivabluedk&parent=streamernews.example.com&muted=true"
-                            height="240"
-                            width="426"
-                            frameborder="0"
-                            scrolling="no"
-                            allowfullscreen="true"></iframe>
+                        <a href="https://twitch.tv/bhfevent" target="_blank"><div style="position: relative; left: 0; top: 0;">
+                                <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_ooclanoo-640x360.jpg" class="bhfback"/>
+                                <img src=".\img\streamers_icons\bhf.png" class="bhflogo"/>
+                            </div></a>
                         </div>
                         <div class="col-sm">
                         <p>t0p0tv</p>
-                            <iframe
-                            src="https://player.twitch.tv/?channel=t0p0tv&parent=streamernews.example.com&muted=true"
-                            height="240"
-                            width="426"
-                            frameborder="0"
-                            scrolling="no"
-                            allowfullscreen="true"></iframe>
+                        <a href="https://twitch.tv/bhfevent" target="_blank"><div style="position: relative; left: 0; top: 0;">
+                                <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_ooclanoo-640x360.jpg" class="bhfback"/>
+                                <img src=".\img\streamers_icons\bhf.png" class="bhflogo"/>
+                            </div></a>
                         </div>
                     </div>
                 </div>
@@ -59,6 +46,38 @@
 
 
     <?php include("./Templates/footer.php") ?>
+    <style>
+    .natback
+    {
+      position: relative;
+      top: 0;
+      left: 0;
+      max-height: 250px;
+    }
+    .natlogo
+    {
+        border-radius: 50%;
+        max-height: 70px;
+      position: absolute;
+      top: 15%;
+      left: 15%;
+    }
+    .bhfback
+    {
+      position: relative;
+      top: 0;
+      left: 0;
+      max-height: 250px;
+    }
+    .bhflogo
+    {
+        border-radius: 50%;
+        max-height: 90px;
+      position: absolute;
+      top: 30%;
+      left: 47.7%;
+    }
+  </style>
     </body>
 
 </html>
